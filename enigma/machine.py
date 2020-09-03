b9bacccdfe8e3d29e993caabd0037a76bd08fe9c
Guilherme Fleiger Felipe <guilhermefliegerfelipe@gmail.com>
2020-09-03 20:20:41 -0300

    Starting main class

---
+++ enigma/machine.py
@@ -0,0 +1,6 @@
+class Machine:
+    def __init__(self):
+        pass
+
+    def type(self, text: str):
+        return text
---
+++ main.py
@@ -0,0 +1,5 @@
+from enigma import Machine
+
+if __name__ == '__main__':
+    machine = Machine()
+    print(machine.type('sometext'))
